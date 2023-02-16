13a38e07d1b8dff278f7e832372aa565c8021cce
Weaboo45 <59713279+Weaboo45@users.noreply.github.com>
2023-02-16 00:29:22 -0600

    minorChange

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,56 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
+ * constants.  This class should not be used for any other purpose.  All constants should be
+ * declared globally (i.e. public static).  Do not put anything functional in this class.
+ *
+ * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * constants are needed, to reduce verbosity.
+ */
+public final class Constants {
+
+    // Drivetrain multipliers
+    public static double ROTATION_DEADBAND = .25;
+    public static double STRAFING_DEADBAND = .25;  //.75
+    public static double SPEED_DEADBAND = .3;
+    public static double MAX_OUTPUT = .75;
+
+    // Drivetrain motor ID ports
+    public static int FRONT_LEFT_TALON_ID = 4;
+    public static int REAR_LEFT_TALON_ID = 1;
+
+    public static int FRONT_RIGHT_TALON_ID = 3;
+    public static int REAR_RIGHT_TALON_ID = 2;
+
+    // Intake arm motor ID ports
+    public static int TOP_SEG_MOTOR_ID = 6;
+    public static int BOTTOM_SEG_MOTOR_ID = 5;
+
+    public static boolean IS_TANKDRIVE_SQUARED = false;
+    public static boolean DOES_CHEESYDRIVE_PIVOT = false;
+    public static boolean IS_ARCADEDRIVE_SQUARED = true;
+
+    // Joystick buttons
+    public static int INTAKE_GRAB_BUTTON = 1;
+    public static int INTAKE_LIFT_BUTTON = 2;
+
+    // Intake arm PID constants
+    public static double INTAKE_KP = 0;
+    public static double INTAKE_KI = 0;
+    public static double INTAKE_KD = 0;
+
+    public static double DESTINATION_POSITION = 0;
+
+    // Intake arm measurements
+    public static double INTAKE_TOP_LENGTH = 24;
+    public static double INTAKE_BOTTOM_LENGTH = 12;
+    public static double TOP_UNITS_PER_METER = 0; 
+}
---
+++ src/main/java/frc/robot/commands/DriveMecanum.java
@@ -0,0 +1,68 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import java.util.function.Supplier;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.MecanumDrivetrain;
+
+
+public class DriveMecanum extends CommandBase {
+  /*
+   * Creates a new DriveMecanum.
+   */
+
+  private MecanumDrivetrain driveTrain;
+  private Supplier<Double>  x, y, z, angle;
+  private Supplier<Rotation2d> r;
+
+  public DriveMecanum(MecanumDrivetrain drivetrain, Supplier<Double> forward, Supplier<Double> strafe, Supplier<Double> zRotation,
+   Supplier<Rotation2d> rAngle, Supplier<Double> theta) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(drivetrain);
+    this.driveTrain = drivetrain;
+    this.x = forward;
+    this.y = strafe;
+    this.z = zRotation;
+    this.r = rAngle;
+    this.angle = theta;
+  }
+
+// Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    double xSpeed = -x.get();
+    double ySpeed = -y.get();
+    double zRotation = z.get();
+    double theta = angle.get();
+    Rotation2d gyroAngle = r.get();
+    
+    driveTrain.driveCartesian(xSpeed, ySpeed, zRotation, gyroAngle.times(-1));
+    //driveTrain.homeBrewMecanum(xSpeed, ySpeed, zRotation, theta);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    driveTrain.driveCartesian(0.0, 0.0, 0.0, r.get());	
+    //driveTrain.homeBrewMecanum(0, 0, 0, 0);
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
